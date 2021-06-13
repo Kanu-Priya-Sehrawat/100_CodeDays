8a6068f3113282da7a42c7ea8fcd87a81b907a4f
Kanu Priya <42824659+Kanu-Priya-Sehrawat@users.noreply.github.com>
2021-06-13 08:25:03 +0530

    Create Remove duplicates from a given string.cpp

---
+++ 13_June/Remove duplicates from a given string.cpp
@@ -0,0 +1,16 @@
+char *removeDuplicate(char str[], int n)
+{
+   int index = 0;
+   for(int i=0; i<n; i++){
+       int j; 
+       for(j=0; j<i; j++){
+           if(str[i] == str[j])
+           break;
+       }
+       if(j==i){
+           str[index++] = str[i];
+       }
+   }
+   return str;
+   
+}
