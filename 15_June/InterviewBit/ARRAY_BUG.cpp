aca3065447b9c2ffcbf4c7a68f461030afdab36f
Kanu Priya <42824659+Kanu-Priya-Sehrawat@users.noreply.github.com>
2021-06-15 21:13:11 +0530

    Create ARRAY_BUG.cpp

---
+++ 15_June/InterviewBit/ARRAY_BUG.cpp
@@ -0,0 +1,14 @@
+// Rotate the array
+// Bhot jyada sundr approach
+
+vector<int> Solution::rotateArray(vector<int> &A, int B) {
+    vector<int> ret; 
+   
+    for (int i = 0; i < A.size(); i++) {
+        
+        ret.push_back(A[(i + B)% A.size()]);
+        
+    }
+    
+    return ret; 
+}
