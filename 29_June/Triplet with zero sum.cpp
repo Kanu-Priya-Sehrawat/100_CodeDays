042713d76a43b5d62bf968339faf6f6f8d401d75
Kanu Priya <42824659+Kanu-Priya-Sehrawat@users.noreply.github.com>
2021-06-29 04:57:59 +0530

    Create Triplet with zero sum.cpp

---
+++ 29_June/Triplet with zero sum.cpp
@@ -0,0 +1,19 @@
+bool findTriplets(int arr[], int n)
+    { 
+        //Your code here
+        sort(arr, arr+n);
+        for(int i=0; i<n-2; i++){
+            int l=i+1, r = n-1;
+            while(l<r){
+                int sum = arr[i] + arr[l] + arr[r];
+                if(sum == 0)
+                return true;
+                
+                if(sum<0)
+                l++;
+                else
+                r--;
+            }
+        }
+        return false;
+    }
